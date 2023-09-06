1271f52dd82e0ad698e9e00229d1b5834deaae5b
Hanna Rogalska <hanrogalska@gmail.com>
2023-09-06 17:56:43 +0200

    hw-6

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,13 +1,42 @@
-const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 
-const { SENDGRID_API_KEY } = process.env;
+const { APP_PASSWORD } = process.env;
+const {MAIL_USER} = process.env
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+const nodemailerConfig = {
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: MAIL_USER,
+    pass: APP_PASSWORD,
+  },
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async (data) => {
-  const mail = { ...data, from: "hanrogalska@gmail.com" };
-  await sgMail.send(mail);
-  return true;
+  const mail = { ...data, from: MAIL_USER };
+
+  try {
+    await transport.sendMail(mail);
+    console.log("Email send success");
+  } catch (error) {
+    console.error(error.message);
+  }
 };
+
+// const sgMail = require("@sendgrid/mail");
+// require("dotenv").config();
+
+// const { SENDGRID_API_KEY } = process.env;
+
+// sgMail.setApiKey(SENDGRID_API_KEY);
+
+// const sendEmail = async (data) => {
+//   const mail = { ...data, from: "hanrogalska@gmail.com" };
+//   await sgMail.send(mail);
+//   return true;
+// };
 
 module.exports = sendEmail;
