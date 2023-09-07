894939a4efca79d2608548fb80972f41d44b9dbd
Hanna Rogalska <hanrogalska@gmail.com>
2023-09-07 13:50:39 +0200

    Update createVerificationEmail.js

--- helpers/createVerificationEmail.js
+++ helpers/createVerificationEmail.js
@@ -4,7 +4,7 @@ const createVerificationEmail = (email, verificationToken) => {
   const mail = {
     to: email,
     subject: "Registration confirmation email",
-    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Email verification</a>`,
+    html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Email verification</a>`,
   };
 
   return mail;
